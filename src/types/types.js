15be4b5553ae033910891bc0501a1ec08fefcd34
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-11 15:14:04 -0500

    creacion de un context y un reducer

---
+++ src/types/types.js
@@ -0,0 +1,6 @@
+// centralizando los strings/opciones del authReducer.
+// Se centralizan en un objeto para evitar errores de tipeo
+export const types = {
+	login: '[auth] login',
+	logout: '[auth] logout',
+};
