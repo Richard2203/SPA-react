fa6d95bafef3aa6abbe920b95f4154a26ddc673f
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-12 17:12:51 -0500

    pruebas en el componente AppRouter

--- src/Tests/routers/AppRouter.test.js
+++ src/Tests/routers/AppRouter.test.js
@@ -1,3 +1,41 @@
+import { mount } from 'enzyme';
+import { AuthContext } from '../../auth/AuthContext';
+import AppRouter from '../../routers/AppRouter';
+
 describe('Pruebas en el componente AppRouter', () => {
-	test('should', () => {});
+	test('debe renderizar el login si no esta autenticado', () => {
+		const contextValue = {
+			user: { logged: false },
+			dispatch: jest.fn(),
+		};
+
+		// hacemos uso de un componente de orden superior por lo cual requerimos
+		// de mount en lugar de shallow
+
+		// AppRouter hace uso de AuthContext y que esta siendo usada por un
+		// useContext por lo cual para enviarle la informacion se pasa mediante
+		// AuthContext.Provider
+		const wrapper = mount(
+			<AuthContext.Provider value={contextValue}>
+				<AppRouter />
+			</AuthContext.Provider>
+		);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	test('debe renderizar todos los componentes si esta autenticado', () => {
+		const contextValue = {
+			user: { logged: true, name: 'paquito' },
+			dispatch: jest.fn(),
+		};
+
+		const wrapper = mount(
+			<AuthContext.Provider value={contextValue}>
+				<AppRouter />
+			</AuthContext.Provider>
+		);
+
+		expect(wrapper.find('nav').exists()).toBe(true);
+	});
 });
